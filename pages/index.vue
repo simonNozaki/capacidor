ade811a76a4cda144e343fd956be7e55521d4ab6
snozaki <snozaki@netprotections.co.jp>
2023-03-08 22:32:37 +0900

    ステータス変更ボタンを1から作り直し...

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@ const addTodo = () => {
   const now = new Date()
   const newTodoItem: TodoItem = {
     id: uuidv4(),
-    title: newTodoTitle.value,
+    title: newTodoTitle.value === '' ? '無題' : newTodoTitle.value,
     status: ['unprocessed', '未対応'],
     description: '',
     createdAt: now,
@@ -29,6 +29,11 @@ const makeTodoDone = (id: string) => {
     // idで消して更新したものを戻す
     todos.value = todos.value.filter(t => t.id !== id)
   }
+}
+
+const getActiveStatusClasses = (isActive: boolean): string[] => {
+  const activeStyle = isActive ? 'todo-list-card-status-button__active' : 'todo-list-card-status-button__inactive'
+  return [activeStyle, 'todo-list-card-status-button']
 }
 </script>
 
@@ -49,30 +54,43 @@ const makeTodoDone = (id: string) => {
             <p class="todo-list-card-caption-title">
               {{ todo.title }}
             </p>
-            <Menu as="div" class="inline-block text-left">
-              <MenuButton as="button">{{ todo.status[1] }}</MenuButton>
-              <MenuItems
-                class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-              >
+            <div class="top-16 w-56">
+              <Menu as="div" class="relative inline-block text-left">
                 <div>
-                  <MenuItem v-slot="{ active }" as="div">
-                    <button :class="[active ? 'todo-list-card-status-button__active' : 'todo-list-card-status-button']">
-                      未対応
-                    </button>
-                  </MenuItem>
-                  <MenuItem v-slot="{ active }" as="div">
-                    <button :class="[active ? 'todo-list-card-status-button__active' : 'todo-list-card-status-button']">
-                      対応中
-                    </button>
-                  </MenuItem>
-                  <MenuItem v-slot="{ active }" as="div">
-                    <button :class="[active ? 'todo-list-card-status-button__active' : 'todo-list-card-status-button']">
-                      対処しない
-                    </button>
-                  </MenuItem>
+                  <MenuButton class="status-menu-button">
+                    {{ todo.status[1] }}
+                  </MenuButton>
                 </div>
-              </MenuItems>
-            </Menu>
+
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+                >
+                  <MenuItems
+                    class="absolute mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <div class="px-1 py-1">
+                      <MenuItem v-slot="{ active }">
+                        <button :class="getActiveStatusClasses(active)">
+                          未対応
+                        </button>
+                      </MenuItem>
+                    </div>
+                    <div class="px-1 py-1">
+                      <MenuItem v-slot="{ active }">
+                        <button :class="getActiveStatusClasses(active)">
+                          対応中
+                        </button>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </div>
           </div>
           <div class="todo-list-card-button">
             <Button @click="makeTodoDone(todo.id)">
@@ -110,11 +128,19 @@ const makeTodoDone = (id: string) => {
   @apply text-lg font-semibold text-stone-800;
 }
 
-.todo-list-card-status-button {
-  @apply flex items-center w-full rounded-md p-2 text-stone-800;
+.status-menu-button {
+  @apply w-full rounded-md text-stone-800 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75;
+}
+
+.todo-list-card-status-button__inactive {
+  @apply text-stone-800;
 }
 
 .todo-list-card-status-button__active {
-  @apply flex items-center w-full rounded-md p-2 bg-teal-800 text-white font-semibold;
+  @apply bg-teal-800 text-white font-semibold;
+}
+
+.todo-list-card-status-button {
+  @apply flex w-full items-center rounded-md px-2 py-2 text-sm;
 }
 </style>
