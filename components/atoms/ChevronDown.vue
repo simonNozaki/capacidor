38bb33d1ae2acb21bff82e53596f3eec5b71cb6f
snozaki <snozaki@netprotections.co.jp>
2023-03-13 21:23:40 +0900

    アイコンコンポーネント

---
+++ components/atoms/ChevronDown.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+export interface Props {
+  size: string;
+}
+withDefaults(defineProps<Props>(), {
+  size: '24'
+})
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 16 16"
+  >
+    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+  </svg>
+</template>
