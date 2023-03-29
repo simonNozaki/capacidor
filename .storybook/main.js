2fa82a2c8b6885a5ac9a19507817981e16e47dd1
snozaki <snozaki@netprotections.co.jp>
2023-03-29 22:01:07 +0900

    feat storybookでもtailwindcssを読み込ませる

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,15 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
+      },
+    },
   ],
   "framework": "@storybook/vue3",
   "core": {
